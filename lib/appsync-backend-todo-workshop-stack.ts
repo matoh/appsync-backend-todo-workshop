77e420930f4f52107227c12a296cd388c0fcff21
Matej Husar <matejhusar@gmail.com>
2023-01-07 17:58:41 +0100

    Added AppSync queries and mutations mapped against DynamoDB

--- lib/appsync-backend-todo-workshop-stack.ts
+++ lib/appsync-backend-todo-workshop-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { Duration, Expiration, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AccountRecovery, CfnUserPoolGroup, UserPool, UserPoolClient, VerificationEmailStyle } from 'aws-cdk-lib/aws-cognito';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import * as path from 'path';
+import { AuthorizationType, FieldLogLevel, GraphqlApi, MappingTemplate, PrimaryKey, SchemaFile, Values } from '@aws-cdk/aws-appsync-alpha';
 
 export class AppsyncBackendTodoWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -45,5 +47,76 @@ export class AppsyncBackendTodoWorkshopStack extends cdk.Stack {
         type: AttributeType.STRING
       }
     });
+
+    const appSyncApi = new GraphqlApi(this, 'TodoAppSyncApi', {
+      name: 'TodoAppSyncApi',
+      schema: SchemaFile.fromAsset(path.join(__dirname, 'schema.graphql')),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.USER_POOL,
+          userPoolConfig: {
+            userPool
+          }
+        },
+        additionalAuthorizationModes: [
+          {
+            authorizationType: AuthorizationType.API_KEY,
+            apiKeyConfig: {
+              name: 'Todo Api Key',
+              description: 'Simple API Key for fetching TODO tasks',
+              expires: Expiration.after(Duration.days(30))
+            }
+          }
+        ]
+      },
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL
+      },
+      xrayEnabled: true
+    });
+
+    // Query: GetTodo
+    appSyncApi.addDynamoDbDataSource('TodoDynamoDbQueryGetTodo', todoDynamoDbTable).createResolver('TodoDynamoDbQueryGetTodoResolver', {
+      typeName: 'Query',
+      fieldName: 'getTodo',
+      requestMappingTemplate: MappingTemplate.dynamoDbGetItem('id', 'id'),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem()
+    });
+
+    // Query: ListTodos
+    appSyncApi.addDynamoDbDataSource('TodoDynamoDbQueryListTodos', todoDynamoDbTable).createResolver('TodoDynamoDbQueryListTodoResolver', {
+      typeName: 'Query',
+      fieldName: 'listTodos',
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList()
+    });
+
+    // Mutation: AddTodo
+    appSyncApi.addDynamoDbDataSource('TodoTableMutationAddTodo', todoDynamoDbTable).createResolver('TodoDynamoDbMutationAddTodoResolver', {
+      typeName: 'Mutation',
+      fieldName: 'addTodo',
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(PrimaryKey.partition('id').auto(), Values.projecting('input')),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem()
+    });
+
+    // Mutation: UpdateTodo
+    appSyncApi
+      .addDynamoDbDataSource('TodoTableMutationUpdateTodo', todoDynamoDbTable)
+      .createResolver('TodoDynamoDbMutationUpdateTodoResolver', {
+        typeName: 'Mutation',
+        fieldName: 'updateTodo',
+        requestMappingTemplate: MappingTemplate.dynamoDbPutItem(PrimaryKey.partition('id').is('input.id'), Values.projecting('input')),
+        responseMappingTemplate: MappingTemplate.dynamoDbResultItem()
+      });
+
+    // Mutation: DeleteTodo
+    appSyncApi
+      .addDynamoDbDataSource('TodoTableMutationDeleteTodo', todoDynamoDbTable)
+      .createResolver('TodoDynamoDbMutationDeleteTodoResolver', {
+        typeName: 'Mutation',
+        fieldName: 'deleteTodo',
+        requestMappingTemplate: MappingTemplate.dynamoDbDeleteItem('id', 'id'),
+        responseMappingTemplate: MappingTemplate.dynamoDbResultItem()
+      });
   }
 }
